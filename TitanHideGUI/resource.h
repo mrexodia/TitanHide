44c14aeafc67ac10c95f3d69604b8f8a7c878b6b
mr.exodia <mr.exodia.tpodt@gmail.com>
2014-01-25 21:32:00 +0100

    - updated readme
    - added TitanHideGUI
    - added TitanHideTest

---
+++ TitanHideGUI/resource.h
@@ -0,0 +1,15 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define DLG_MAIN                                100
+#define IDC_CHK_NTCLOSE                         1001
+#define IDC_CHK_PROCESSDEBUGFLAGS               1009
+#define IDC_CHK_PROCESSDEBUGOBJECTHANDLE        1012
+#define IDC_CHK_PROCESSDEBUGPORT                1013
+#define IDC_CHK_DEBUGOBJECT                     1014
+#define IDC_EDT_PID                             1016
+#define IDC_BTN_UNHIDE                          1018
+#define IDC_BTN_HIDE                            1019
+#define IDC_BTN_UNHIDEALL                       1021
+#define IDC_CHK_SYSTEMDEBUGGERINFORMATION       1022
