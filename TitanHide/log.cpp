5f8dc41e095d566274eb4d8e2eca67ffb5a8185f
mr.exodia <mr.exodia.tpodt@gmail.com>
2014-02-03 20:12:21 +0100

    - cleaned up the log.cpp file

--- TitanHide/log.cpp
+++ TitanHide/log.cpp
@@ -7,48 +7,40 @@ void Log(const char* format, ...)
     char msg[1024]="";
     va_list vl;
     va_start(vl, format);
-    if(_vsnprintf(msg, sizeof(msg)/sizeof(char), format, vl))
-        DbgPrint(msg);
+    _vsnprintf(msg, sizeof(msg)/sizeof(char), format, vl);
+#ifdef _DEBUG
+    DbgPrint(msg);
+#endif
     va_end(format);
-
-    UNICODE_STRING     uniName;
-    OBJECT_ATTRIBUTES  objAttr;
-
-    RtlInitUnicodeString(&uniName, L"\\DosDevices\\C:\\TitanHide.log");
-    InitializeObjectAttributes(&objAttr, &uniName,
-                               OBJ_CASE_INSENSITIVE | OBJ_KERNEL_HANDLE,
+    UNICODE_STRING FileName;
+    OBJECT_ATTRIBUTES objAttr;
+    RtlInitUnicodeString(&FileName, L"\\DosDevices\\C:\\TitanHide.log");
+    InitializeObjectAttributes(&objAttr, &FileName,
+                               OBJ_CASE_INSENSITIVE|OBJ_KERNEL_HANDLE,
                                NULL, NULL);
-
-    HANDLE   handle;
-    NTSTATUS ntstatus;
-    IO_STATUS_BLOCK    ioStatusBlock;
-
-    // Do not try to perform any file operations at higher IRQL levels.
-    // Instead, you may use a work item or a system worker thread to perform file operations.
-
-    if(KeGetCurrentIrql() != PASSIVE_LEVEL)
+    if(KeGetCurrentIrql()!=PASSIVE_LEVEL)
+    {
+#ifdef _DEBUG
+        DbgPrint("[TITANHIDE] KeGetCurrentIrql != PASSIVE_LEVEL!\n");
+#endif
         return;
-
-    ntstatus = ZwCreateFile(&handle,
-                            FILE_APPEND_DATA,
-                            &objAttr, &ioStatusBlock, NULL,
-                            FILE_ATTRIBUTE_NORMAL,
-                            FILE_SHARE_WRITE | FILE_SHARE_READ,
-                            FILE_OPEN_IF,
-                            FILE_SYNCHRONOUS_IO_NONALERT,
-                            NULL, 0);
-    size_t  cb;
-
+    }
+    HANDLE handle;
+    IO_STATUS_BLOCK ioStatusBlock;
+    NTSTATUS ntstatus=ZwCreateFile(&handle,
+                                   FILE_APPEND_DATA,
+                                   &objAttr, &ioStatusBlock, NULL,
+                                   FILE_ATTRIBUTE_NORMAL,
+                                   FILE_SHARE_WRITE|FILE_SHARE_READ,
+                                   FILE_OPEN_IF,
+                                   FILE_SYNCHRONOUS_IO_NONALERT,
+                                   NULL, 0);
     if(NT_SUCCESS(ntstatus))
     {
-        ntstatus = RtlStringCbLengthA(msg, sizeof(msg), &cb);
+        size_t cb;
+        ntstatus=RtlStringCbLengthA(msg, sizeof(msg), &cb);
         if(NT_SUCCESS(ntstatus))
-        {
-            ntstatus = ZwWriteFile(handle, NULL, NULL, NULL, &ioStatusBlock,
-                                   msg, cb, NULL, NULL);
-        }
+            ZwWriteFile(handle, NULL, NULL, NULL, &ioStatusBlock, msg, cb, NULL, NULL);
         ZwClose(handle);
     }
-
-
 }
