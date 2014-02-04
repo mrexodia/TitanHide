5bdd9f2a2ffd83ef5f03891e09ef46a656b23683
mr.exodia <mr.exodia.tpodt@gmail.com>
2014-02-04 22:55:48 +0100

    - removed locking library (replaced with Interlocked* functions)
    - no more allocations (caused a BAD_POOL_HEADER bugcheck)

--- TitanHide/hider.cpp
+++ TitanHide/hider.cpp
@@ -1,136 +1,81 @@
 #include "hider.h"
 #include "misc.h"
 
-static HIDE_ENTRY* HideEntries=0;
-static int TotalHideEntries=0;
+#define MAX_HIDE_ENTRIES 65536
 
-//simple locking library
-static volatile bool locked=false;
-
-static void lock()
-{
-    while(locked);
-    locked=true;
-}
-
-static void unlock()
-{
-    locked=false;
-}
+static HIDE_ENTRY HideEntries[MAX_HIDE_ENTRIES];
+static LONG TotalHideEntries=0;
 
 //entry management
 static void EntryAdd(HIDE_ENTRY* NewEntry)
 {
-    lock();
-    int NewTotalHideEntries=TotalHideEntries+1;
-    HIDE_ENTRY* NewHideEntries=(HIDE_ENTRY*)RtlAllocateMemory(true, NewTotalHideEntries*sizeof(HIDE_ENTRY));
-    if(!NewHideEntries)
-    {
-        unlock();
+    if(TotalHideEntries>=MAX_HIDE_ENTRIES || !HideEntries)
         return;
-    }
-    RtlCopyMemory(&NewHideEntries[0], &HideEntries[0], TotalHideEntries*sizeof(HIDE_ENTRY));
-    RtlCopyMemory(&NewHideEntries[TotalHideEntries], &NewEntry[0], sizeof(HIDE_ENTRY));
-    if(HideEntries)
-        RtlFreeMemory(HideEntries);
-    HideEntries=NewHideEntries;
-    TotalHideEntries=NewTotalHideEntries;
-    unlock();
+    RtlCopyMemory(&HideEntries[TotalHideEntries], &NewEntry[0], sizeof(HIDE_ENTRY));
+    InterlockedAdd(&TotalHideEntries, 1);
 }
 
 static void EntryClear()
 {
-    lock();
-    TotalHideEntries=0;
-    if(HideEntries)
-        RtlFreeMemory(HideEntries);
-    HideEntries=0;
-    unlock();
+    InterlockedExchange(&TotalHideEntries, 0);
 }
 
 static void EntryDel(int EntryIndex)
 {
-    lock();
     if(EntryIndex<TotalHideEntries && HideEntries)
     {
         int NewTotalHideEntries=TotalHideEntries-1;
         if(!NewTotalHideEntries) //nothing left
         {
-            unlock();
             EntryClear();
             return;
         }
-        HIDE_ENTRY* NewHideEntries=(HIDE_ENTRY*)RtlAllocateMemory(true, NewTotalHideEntries*sizeof(HIDE_ENTRY));
-        if(!NewHideEntries)
-        {
-            unlock();
-            return;
-        }
         if(!EntryIndex)
-            RtlCopyMemory(&NewHideEntries[0], &HideEntries[1], NewTotalHideEntries*sizeof(HIDE_ENTRY));
+            RtlCopyMemory(&HideEntries[0], &HideEntries[1], NewTotalHideEntries*sizeof(HIDE_ENTRY));
         else
         {
-            RtlCopyMemory(&NewHideEntries[0], &HideEntries[0], EntryIndex*sizeof(HIDE_ENTRY));
-            RtlCopyMemory(&NewHideEntries[EntryIndex], &HideEntries[EntryIndex+1], (NewTotalHideEntries-EntryIndex)*sizeof(HIDE_ENTRY));
+            RtlCopyMemory(&HideEntries[EntryIndex], &HideEntries[EntryIndex+1], (NewTotalHideEntries-EntryIndex)*sizeof(HIDE_ENTRY));
         }
-        RtlCopyMemory(NewHideEntries, HideEntries, TotalHideEntries*sizeof(HIDE_ENTRY));
-        RtlFreeMemory(HideEntries);
-        HideEntries=NewHideEntries;
-        TotalHideEntries=NewTotalHideEntries;
+        InterlockedExchange(&TotalHideEntries, NewTotalHideEntries);
     }
-    unlock();
 }
 
 static int EntryFind(ULONG Pid)
 {
-    lock();
-    if(!HideEntries)
-    {
-        unlock();
-        return -1;
-    }
     for(int i=0; i<TotalHideEntries; i++)
     {
         if(HideEntries[i].Pid==Pid)
         {
-            unlock();
             return i;
         }
     }
-    unlock();
     return -1;
 }
 
 static ULONG EntryGet(int EntryIndex)
 {
-    lock();
     ULONG Type=0;
     if(EntryIndex<TotalHideEntries && HideEntries)
     {
         Type=HideEntries[EntryIndex].Type;
     }
-    unlock();
     return Type;
 }
 
 static void EntrySet(int EntryIndex, ULONG Type)
 {
-    lock();
     if(EntryIndex<TotalHideEntries && HideEntries)
     {
         HideEntries[EntryIndex].Type|=Type;
     }
-    unlock();
 }
 
 static void EntryUnset(int EntryIndex, ULONG Type)
 {
-    lock();
     if(EntryIndex<TotalHideEntries && HideEntries)
     {
         HideEntries[EntryIndex].Type&=~Type;
     }
-    unlock();
 }
 
 //usable functions
