15a3fa7b4d5ad5dd911e43661e0f1eba796933ff
Mattiwatti <mattiwatti@gmail.com>
2019-03-13 00:39:37 +0100

    Add Resharper printf argument type checking and formatting to Log()

--- TitanHide/log.h
+++ TitanHide/log.h
@@ -3,6 +3,16 @@
 
 #include "_global.h"
 
-void Log(const char* format, ...);
+#if defined(__RESHARPER__)
+#define PRINTF_ATTR(FormatIndex, FirstToCheck) \
+    [[gnu::format(printf, FormatIndex, FirstToCheck)]]
+#elif defined(__GNUC__)
+#define PRINTF_ATTR(FormatIndex, FirstToCheck) \
+    __attribute__((format(printf, FormatIndex, FirstToCheck)))
+#else
+#define PRINTF_ATTR(FormatIndex, FirstToCheck)
+#endif
+
+PRINTF_ATTR(1, 2) void Log(const char* format, ...);
 
 #endif
