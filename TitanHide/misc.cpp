a584b7100ee41b1e1222917174a97759dc4d98b6
Mattiwatti <mattiwatti@gmail.com>
2019-03-24 19:49:30 +0100

    Revert dcaceb9. Fixes #35

--- TitanHide/misc.cpp
+++ TitanHide/misc.cpp
@@ -3,8 +3,9 @@
 
 ULONG Misc::GetProcessIDFromProcessHandle(HANDLE ProcessHandle)
 {
+    // FIXME: this causes a Driver Verifier violation when non-kernel handles are passed to ObReferenceObjectByHandle inside ZwQIP
     PROCESS_BASIC_INFORMATION PBI;
-    if(NT_SUCCESS(Undocumented::NtQueryInformationProcess(ProcessHandle, ProcessBasicInformation, &PBI, sizeof(PBI), NULL)))
+    if(NT_SUCCESS(Undocumented::ZwQueryInformationProcess(ProcessHandle, ProcessBasicInformation, &PBI, sizeof(PBI), NULL)))
         return (ULONG)PBI.UniqueProcessId;
 
     return 0;
