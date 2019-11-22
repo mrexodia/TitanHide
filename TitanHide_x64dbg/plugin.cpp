ff0283bf0786f17651ec28a73ae0a311cf1fcd53
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2019-11-22 15:25:04 +0100

    separate TitanHide and TitanUnhide commands

--- TitanHide_x64dbg/plugin.cpp
+++ TitanHide_x64dbg/plugin.cpp
@@ -41,34 +41,9 @@ static bool TitanHideCall(HIDE_COMMAND Command)
     return result;
 }
 
-PLUG_EXPORT void CBCREATEPROCESS(CBTYPE cbType, PLUG_CB_CREATEPROCESS* info)
-{
-    pid = info->fdProcessInfo->dwProcessId;
-}
-
-PLUG_EXPORT void CBATTACH(CBTYPE cbType, PLUG_CB_ATTACH* info)
-{
-    pid = info->dwProcessId;
-}
-
-PLUG_EXPORT void CBSYSTEMBREAKPOINT(CBTYPE cbType, PLUG_CB_SYSTEMBREAKPOINT* info)
-{
-    DbgCmdExecDirect("TitanHide");
-}
-
-PLUG_EXPORT void CBSTOPDEBUG(CBTYPE cbType, PLUG_CB_STOPDEBUG* info)
-{
-    if(hidden)
-    {
-        hidden = false;
-        _plugin_logprintf("[" PLUGIN_NAME "] Unhiding PID %X (%ud)\n", pid, pid);
-        TitanHideCall(UnhidePid);
-    }
-}
-
 static bool cbTitanHide(int argc, char* argv[])
 {
-    if(!hidden)
+    if (!hidden)
     {
         _plugin_logprintf("[" PLUGIN_NAME "] Hiding PID %X (%ud)\n", pid, pid);
         if (TitanHideCall(HidePid))
@@ -77,12 +52,18 @@ static bool cbTitanHide(int argc, char* argv[])
             hidden = true;
         }
     }
-    else
+    return hidden;
+}
+
+static bool cbTitanUnhide(int argc, char* argv[])
+{
+    if (hidden)
     {
         _plugin_logprintf("[" PLUGIN_NAME "] Unhiding PID %X (%ud)\n", pid, pid);
-        TitanHideCall(UnhidePid);
+        if (TitanHideCall(UnhidePid))
+            hidden = false;
     }
-    return true;
+    return !hidden;
 }
 
 static bool cbTitanHideOptions(int argc, char* argv[])
@@ -102,16 +83,38 @@ static bool cbTitanHideOptions(int argc, char* argv[])
     return true;
 }
 
+PLUG_EXPORT void CBCREATEPROCESS(CBTYPE cbType, PLUG_CB_CREATEPROCESS* info)
+{
+    pid = info->fdProcessInfo->dwProcessId;
+}
+
+PLUG_EXPORT void CBATTACH(CBTYPE cbType, PLUG_CB_ATTACH* info)
+{
+    pid = info->dwProcessId;
+}
+
+PLUG_EXPORT void CBSYSTEMBREAKPOINT(CBTYPE cbType, PLUG_CB_SYSTEMBREAKPOINT* info)
+{
+    char* argv = "TitanHide";
+    cbTitanHide(1, &argv);
+}
+
+PLUG_EXPORT void CBSTOPDEBUG(CBTYPE cbType, PLUG_CB_STOPDEBUG* info)
+{
+    char* argv = "TitanUnhide";
+    cbTitanUnhide(1, &argv);
+}
+
 void TitanHideInit(PLUG_INITSTRUCT* initStruct)
 {
-    if(!_plugin_registercommand(pluginHandle, "TitanHide", cbTitanHide, false))
-        puts("[" PLUGIN_NAME "] Error registering command 'TitanHide'!");
-    if (!_plugin_registercommand(pluginHandle, "TitanHideOptions", cbTitanHideOptions, false))
-        puts("[" PLUGIN_NAME "] Error registering command 'TitanHideOptions'!");
+    _plugin_registercommand(pluginHandle, "TitanHide", cbTitanHide, true);
+    _plugin_registercommand(pluginHandle, "TitanUnhide", cbTitanUnhide, true);
+    _plugin_registercommand(pluginHandle, "TitanHideOptions", cbTitanHideOptions, false);
 }
 
 void TitanHideStop()
 {
     _plugin_unregistercommand(pluginHandle, "TitanHideOptions");
+    _plugin_unregistercommand(pluginHandle, "TitanUnhide");
     _plugin_unregistercommand(pluginHandle, "TitanHide");
 }
