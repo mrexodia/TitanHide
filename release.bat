7ac3b1b731c398ad8eea61a5886d4b2d78be0d35
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2021-10-27 17:42:20 +0200

    Fix solution directory structure and release script

--- release.bat
+++ release.bat
@@ -1,13 +1,7 @@
 @echo off
 rmdir /S /Q bin
-mkdir bin\x32\plugins
-mkdir bin\x64\plugins
+mkdir bin\x32
+mkdir bin\x64
 
-copy Win7Release\*.* bin\x32\
-copy Release\*.exe bin\x32\
-copy Release\*.dll bin\x32\plugins\
-copy Release\TitanHide.dp32 bin\x32\plugins\
-copy x64\Win7Release\*.* bin\x64\
-copy x64\Release\*.exe bin\x64\
-copy x64\Release\*.dll bin\x64\plugins\
-copy x64\Release\TitanHide.dp64 bin\x64\plugins\
+xcopy /s /q Release\* bin\x32\
+xcopy /s /q x64\Release\* bin\x64\
